5a594080a912278e52e140ed92d3d358a33e3845
UUTAN <uutan@qq.com>
2016-05-17 12:53:06 +0800

    回调

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -57,83 +57,11 @@ class SiteController extends Controller
     public function actionGet()
     {
         $oss = \Yii::$app->oss;
-        $callbackUrl = Url::to(['/site/callback'],true);
+        $callbackUrl = Url::to(['/aliyun/oss'],true);
         $response = $oss->getSignature('user-dir/', $callbackUrl,30);
         echo $response;
     }
 
-
-    /**
-     * 回调数据保存
-     */
-    public function actionCallback()
-    {
-        $this->enableCsrfValidation = false;
-
-        // 1.获取OSS的签名header和公钥url header
-        $authorizationBase64 = "";
-        $pubKeyUrlBase64 = "";
-        if (isset($_SERVER['HTTP_AUTHORIZATION']))
-        {
-            $authorizationBase64 = $_SERVER['HTTP_AUTHORIZATION'];
-        }
-        if (isset($_SERVER['HTTP_X_OSS_PUB_KEY_URL']))
-        {
-            $pubKeyUrlBase64 = $_SERVER['HTTP_X_OSS_PUB_KEY_URL'];
-        }
-
-        // 没有拿到授权
-        if ($authorizationBase64 == '' || $pubKeyUrlBase64 == '')
-        {
-            Yii::$app->end(403);
-        }
-
-        // 2.获取OSS的签名
-        $authorization = base64_decode($authorizationBase64);
-
-        // 3.获取公钥
-        $pubKeyUrl = base64_decode($pubKeyUrlBase64);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $pubKeyUrl);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-        $pubKey = curl_exec($ch);
-        if ($pubKey == "")
-        {
-            Yii::$app->end(403);
-        }
-
-        // 4.获取回调body
-        $body = file_get_contents('php://input');
-        Yii::trace(var_export($body,true));
-
-        // 5.拼接待签名字符串
-        $authStr = '';
-        $path = $_SERVER['REQUEST_URI'];
-        $pos = strpos($path, '?');
-        if ($pos === false)
-        {
-            $authStr = urldecode($path)."\n".$body;
-        }
-        else
-        {
-            $authStr = urldecode(substr($path, 0, $pos)).substr($path, $pos, strlen($path) - $pos)."\n".$body;
-        }
-
-        // 6.验证签名
-        $ok = openssl_verify($authStr, $authorization, $pubKey, OPENSSL_ALGO_MD5);
-        if ($ok == 1)
-        {
-            header("Content-Type: application/json");
-            $data = array("Status"=>"Ok");
-            echo json_encode($data);
-        }
-        else
-        {
-            Yii::$app->end(403);
-        }
-
-    }
 
 
 }
